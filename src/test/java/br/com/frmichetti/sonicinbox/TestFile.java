e6c6518916c019b96e4c5d2b515dd69e742a8b7d
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2016-12-08 21:20:59 -0200

    update Pom File

--- src/test/java/br/com/frmichetti/sonicinbox/TestFile.java
+++ src/test/java/br/com/frmichetti/sonicinbox/TestFile.java
@@ -3,14 +3,18 @@ package br.com.frmichetti.sonicinbox;
 import java.io.File;
 import java.net.URL;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestFile {
 
-	@Test
+	private URL url;
+	
+	@Before
 	public void startupPath() throws Exception {
 		
-		URL url = TestFile.class.getResource("./teste.txt");
+		url = new URL("file:///teste.txt");				
 		
 		System.out.println(url);
 		
@@ -29,6 +33,11 @@ public class TestFile {
 		
 		System.out.println(url);
 
+	}
+	
+	@Test
+	public void test(){
+		Assert.assertNotNull(url);
 	}
 
 }
